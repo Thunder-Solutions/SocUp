cdb9f83200fe0f5c56692eedf013cd3eaf186de0
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-06-22 19:55:26 -0400

    updating business wording

--- src/locales/for-business.ts
+++ src/locales/for-business.ts
@@ -6,6 +6,6 @@ export default {
     questsTitle: 'Quests',
     questsDesc: 'Employee facing businesses will be able to give quests for users who make purchases.',
     levelUpTitle: 'Level Up',
-    levelUpDesc: 'Businesses earn additional perks as organizations have the ability to [missing content]'
+    levelUpDesc: 'Businesses earn additional perks as organizations have the ability to level up as well.'
   },
 }
