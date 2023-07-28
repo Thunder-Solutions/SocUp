7bae4a2975ab63c676407ed38819d2a7cbca16c7
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-07-28 19:17:46 -0400

    updating content

--- src/locales/for-business.ts
+++ src/locales/for-business.ts
@@ -2,9 +2,9 @@ export default {
   'en-US': {
     title: 'Business Participation',
     freeFeaturesTitle: 'Free Features',
-    freeFeaturesDesc: 'Job Board, rewards board, and business bargaining are all free features.',
+    freeFeaturesDesc: 'Job Board, rewards board, and business bargaining are available for select businesses.',
     questsTitle: 'Quests',
-    questsDesc: 'Employee facing businesses will be able to give quests for users who make purchases.',
+    questsDesc: 'Customer facing businesses will be able to give quests for users who make purchases.',
     levelUpTitle: 'Level Up',
     levelUpDesc: 'Businesses earn additional perks as organizations have the ability to level up as well.'
   },
