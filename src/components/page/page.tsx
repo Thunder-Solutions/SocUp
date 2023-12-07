061df3ce14ab9f219041cb0a5908aa644fd8b378
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-12-06 21:34:38 -0500

    adding parallax provider to fix error

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -1,16 +1,19 @@
 import { DivTagProps } from '@/utilities';
 import css from './page.module.css';
 import { SiteFooter, SiteHeader } from '@/components';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 export type PageProps = DivTagProps;
 
 const Page = ({ children, id, ...props }: PageProps) => {
 	return (
-		<div {...props} className={css.page} id={id}>
-			<SiteHeader pageId={id} />
-			<main className={css.main}>{children}</main>
-			<SiteFooter />
-		</div>
+		<ParallaxProvider>
+			<div {...props} className={css.page} id={id}>
+				<SiteHeader pageId={id} />
+				<main className={css.main}>{children}</main>
+				<SiteFooter />
+			</div>
+		</ParallaxProvider>
 	);
 };
 
